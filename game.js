9f923ff45e314b20e50105d7807778834980446a
Kevin Sheard <kevinsheard@outlook.de>
2021-12-31 21:23:44 +0100

    [in work] several changes

--- game.js
+++ game.js
@@ -1,16 +1,258 @@
-module.exports.gameLoop = (io, room) => {
-    console.log('gameloop here');
-    io.to(room).emit('initiateGame');
+'use strict';
 
-    // const players = io.sockets.clients(room); 
+module.exports = class Game {
+    MoveNumber = 0;
+    playerFirstMove = false;
+    playerToMove = null;
+    currentOpponent = null;
+    columns = 8;
+	rows = 8;
+	figuresField = new Array(this.columns);
 
-    const tenSeconds = 10000;
+	constructor(room, players, io) {
+        this.room = room;
+        this.players = players;
+        this.io = io;
 
-    setTimeout(() => {
-        io.to(room).emit('timeover');    
-    }, tenSeconds);
+		for (let row = 0; row < this.figuresField.length; row++) {
+			this.figuresField[row] = new Array(this.columns);	
+		}
 
-    // io.to(room).once('chooseFirstMove', (obj) => {
-    //     // player.to(room).emit('opponentChoosed');
-    // });
+		this.gameField = new Array(this.columns);
+
+		for (let rows = 0; rows < this.gameField.length; rows++) {
+		    this.gameField[rows] = new Array(this.columns);
+
+			for (let columns = 0; columns < this.gameField[rows].length; columns++) {
+				this.gameField[rows][columns] = {
+					place: {
+						column: columns, 
+						row: rows
+					},
+					player: null,
+					playerRevealed: false,
+					freeSpace: null,
+					figure: null
+				}
+			}
+		}
+
+		this.gameField.forEach((column, columnIndex) => {
+			column.forEach((row, rowIndex) => {
+				if(columnIndex === this.columns - 2 || columnIndex === this.columns - 1) {
+					this.gameField[columnIndex][rowIndex].player = players[0];
+				}else if(columnIndex === 0 || columnIndex === 1) {
+					this.gameField[columnIndex][rowIndex].player = players[1];
+				}else {
+					this.gameField[columnIndex][rowIndex].freeSpace = true;
+				}
+			});
+		});
+	}
+
+    invertFieldForPlayer2({row, column}, onlyOnPlayerToMove) {
+        if(!onlyOnPlayerToMove || this.playerToMove === this.players[2]) {
+            return {
+                row: (row * -1) + (this.rows - 1),
+                column: (column * -1) + (this.columns - 1)
+            }
+        }
+
+        return {
+            row: row,
+            column: column
+        }
+    }
+
+	start() {
+		this.io.to(this.room).emit('start');
+	}
+
+	end() {
+		this.io.to(this.room).emit('end');
+	}
+
+	toMove() {
+		this.MoveNumber++;
+		return this.MoveNumber % this.players.length; // 0, 1, 0, 1... abwechselnd
+	}
+
+  	setFigures(caller, figures) {
+		//   console.log(figures);
+		// let figuresField = new Array(this.columns);
+
+		// console.table(this.figuresField);
+
+		let player1Counter = 0;
+		let player2Counter = figures.length - 1;
+		for (let rows = 0; rows < this.figuresField.length; rows++) {
+			for (let columns = 0; columns < this.figuresField[rows].length; columns++) {
+				if(this.gameField[rows][columns].player === caller.id) {
+					if(caller.id === this.players[0]) {
+						this.figuresField[rows][columns] = figures[player1Counter];
+						player1Counter++;
+					}else {
+						this.figuresField[rows][columns] = figures[player2Counter];
+						player2Counter--;
+					}
+				}
+			}
+		}
+
+		console.table(this.figuresField);
+
+		player1Counter = 0;
+		player2Counter = figures.length - 1;
+		for (let rows = 0; rows < this.gameField.length; rows++) {
+			for (let columns = 0; columns < this.gameField[rows].length; columns++) {
+				if(this.gameField[rows][columns].player === caller.id) {
+					if(caller.id === this.players[0]) {
+						this.gameField[rows][columns].figure = figures[player1Counter];
+						player1Counter++;
+					}else {
+						this.gameField[rows][columns].figure = figures[player2Counter];
+						player2Counter--;
+					}
+				}
+			}
+		}
+  	}
+
+	firstMove(caller, figure) {
+        // console.log(caller.id);
+        // console.log(figure);
+		if(!this.playerFirstMove) {
+			// console.log('first');
+			this.playerFirstMove = figure;
+            this.currentOpponent = caller.id;
+		}else {            
+			if(this.playerFirstMove === figure) {
+				this.playerFirstMove = null;
+                this.currentOpponent = null;
+				this.io.to(this.room).emit('firstMoveHas', null);
+			}else if(
+				(this.playerFirstMove === 'rock' && figure === 'paper') ||
+				(this.playerFirstMove === 'paper' && figure === 'scissor') ||
+				(this.playerFirstMove === 'scissor' && figure === 'rock')
+			) {
+				// console.log('caller won');
+                this.playerToMove = caller.id;
+                this.io.to(this.room).emit('firstMoveHas', caller.id);
+			}else {
+				// console.log('caller lost');
+                this.playerToMove = this.currentOpponent;
+                this.currentOpponent = caller.id;
+                this.io.to(this.room).emit('firstMoveHas', this.playerToMove);
+			}
+		}
+  	}
+
+	getNextFields(playerPos, field) {
+        // console.log(playerPos);
+        // console.log(field);
+		if(
+			(playerPos.row === field.row - 1 && playerPos.column === field.column) ||
+			(playerPos.row === field.row + 1 && playerPos.column === field.column) ||
+			(playerPos.row === field.row && playerPos.column === field.column - 1) ||
+			(playerPos.row === field.row && playerPos.column === field.column + 1)
+		) {
+			return true;
+		}
+
+		return false;
+    }
+
+	movePlayer(caller, {figure, playerPos, field}) {
+		// player that is to move
+		const player = this.playerToMove;
+        const opponent = this.currentOpponent;
+
+		console.log('hier');
+
+        playerPos = this.invertFieldForPlayer2(playerPos, true);
+        field = this.invertFieldForPlayer2(field, true);
+
+		// disallow if the allowed player is not the caller
+		if (player !== caller.id) {
+			console.log("Player not caller");
+			return;
+		}
+
+		if(!this.gameField[playerPos.row][playerPos.column].player === caller.id) {
+			console.log('other self');
+			return;
+		}
+
+		// for (let i = 0; i < this.gameField.length; i++) {
+		// 	for (let j = 0; j < this.gameField[i].length; j++) {
+		// 		console.log(this.gameField[i][j].figure);
+		// 	}
+		// }
+
+		// console.log(this.getNextFields(playerPos, field));
+
+		// if(this.getNextFields(playerPos, field)) {
+		// 	if(this.gameField[playerPos.row][playerPos.column].player) {
+		// 		caller.emit('moveAllowed', true);
+		// 	}
+		// }
+
+		if(this.getNextFields(playerPos, field)) {
+			if(this.gameField[playerPos.row][playerPos.column].player) {
+				if(this.gameField[field.row][field.column].player !== caller && this.gameField[field.row][field.column].player) {
+					//Angriff
+					const opponentFigure = this.gameField[field.row][field.column].figure;
+					if(figure === opponentFigure) {
+						console.log('Angriff pat');
+						//send Pat
+					}else if(
+						(opponentFigure === 'rock' && figure === 'paper') ||
+						(opponentFigure === 'paper' && figure === 'scissor') ||
+						(opponentFigure === 'scissor' && figure === 'rock')
+					) {
+						//Angriff gewonnen
+						console.log('Angriff gewonnen');
+						caller.broadcast.emit('opponentMoveTo', [
+                            this.invertFieldForPlayer2(playerPos, false), 
+                            this.invertFieldForPlayer2(field, false)
+                        ]);
+					}else {
+						//Angriff verloren
+                        caller.emit('moveAllowed', false);
+						this.io.to(this.room).emit('callerLost', {
+                            caller: player,
+                            from: playerPos, 
+                            to: field,
+							fromInverted: this.invertFieldForPlayer2(playerPos, false),
+							toInverted: this.invertFieldForPlayer2(field, false),
+							opponent: opponentFigure
+                        });
+                        return;
+					}
+				}else {
+                    // console.log('kein Angriff');
+					//kein Angriff
+					this.gameField[field.row][field.column].player = caller;
+					this.gameField[field.row][field.column].figure = figure;
+					// this.gameField[field.row][field.column].freeSpace = false;
+
+					this.gameField[playerPos.row][playerPos.column].player = null;
+					this.gameField[playerPos.row][playerPos.column].figure = null;
+					// this.gameField[playerPos.row][playerPos.column].freeSpace = true;
+
+                    // console.log(playerPos);
+                    // console.log(this.invertFieldForPlayer2(playerPos.row, playerPos.column));
+
+					caller.broadcast.emit('opponentMoveTo', [
+                        this.invertFieldForPlayer2(playerPos, false), 
+                        this.invertFieldForPlayer2(field, false)
+                    ]);
+				}
+
+                caller.emit('moveAllowed', true);
+			}
+		}
+
+		// console.table(this.figuresField);
+	}
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const { connectPlayers, gameLoop } = require('./game');
+const Game = require('./game');
 
 const io = new Server(server, {
   cors: {
@@ -12,165 +12,46 @@ const io = new Server(server, {
   }
 });
 
-let roomCounter = 1;
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-class Game {
-  constructor(room, players) {
-    this.room = room;
-    this.players = players;
-    this.MoveNumber = 0;
-
-    console.log("Room", this.room);
-    console.log("Players", this.players);
-    console.log("MoveNumber", this.MoveNumber);
-
-    const columns = 8;
-    const rows = 8;
-
-    this.gameField = new Array(columns);
-
-    for (let rows = 0; rows < this.gameField.length; rows++) {
-      this.gameField[rows] = new Array(columns);
-
-      for (let columns = 0; columns < this.gameField[rows].length; columns++) {
-        this.gameField[rows][columns] = {
-            place: {
-              column: columns, 
-              row: rows
-            },
-            player1: false,
-            player2: false,
-            playerRevealed: false,
-            freeSpace: false,
-            figure: null
-        }
-      }
-    }
-
-    this.gameField.forEach((column, columnIndex) => {
-      column.forEach((row, rowIndex) => {
-          if(columnIndex === columns - 2 || columnIndex === columns - 1) {
-              this.gameField[columnIndex][rowIndex].player1 = true;
-              // this.gameField[columnIndex][rowIndex].playerSoldier = 'paper';
-          }else if(columnIndex === 0 || columnIndex === 1) {
-              this.gameField[columnIndex][rowIndex].player2 = true;
-          }else {
-              this.gameField[columnIndex][rowIndex].freeSpace = true;
-          }
-      });
-    });
-
-    // console.table(this.gameField);
-  }
-
-  start() {
-    // starte das game
-    io.to(this.room).emit('start');
-  }
-
-  end() {
-    // sag allen clients das game ist vorbei
-    io.to(this.room).emit('end');
-  }
-
-  movePiece(piece, position) {
-    // send players the information that some piece has been moved
-    io.to(this.room).emit('move', { piece, position });
-  }
-
-  // data ist default leer, um error handling manuell zu machen
-  gameStep(caller, data = {}) {
-    // player that is to move
-    const player = this.players[this.toMove()];
-
-    // disallow if the allowed player is not the caller
-    if (player !== caller) {
-      return;
-    }
-
-    // example events handled
-
-    if (data.type === 'move') {
-      this.movePiece(data.piece, data.position);
-    }
-
-    else if (data.type === 'forfeit') {
-      io.to(this.room).emit('forfeit', { "player": caller });
-      this.end();
-    }
-
-    // notify the other player he can now do whatever
-    if (player.socketId !== caller) {
-      io.to(caller).emit('yourTurn');
-    }
-  }
-
-  toMove() {
-    this.MoveNumber++;
-    return this.MoveNumber % this.players.length; // 0, 1, 0, 1... abwechselnd
-  }
-};
-
+let roomCounter = 1;
 let games = {};
 
 io.on('connection', (socket) => {
+	let room = `room${roomCounter}`;
 
-  let room = `room${roomCounter}`;
+	socket.join(room);
+	socket.send({"room": roomCounter});
 
-  socket.join(room);
-  socket.send({"room": roomCounter});
+	if(io.sockets.adapter.rooms.get(room).size > 1) {
+		roomCounter++;
 
-  if(io.sockets.adapter.rooms.get(room).size > 1) {
-    roomCounter++;
-    
-    // get users for the room
-    let users = io.sockets.adapter.rooms.get(room);
+		let users = io.sockets.adapter.rooms.get(room);
 
-    //convert stupig map to goody array
-    const players = [[...users][0], [...users][1]];
+		//convert stupid map to goody array
+		const players = [[...users][0], [...users][1]];
 
-    // debug output
-    // console.log(room);
-    // console.log(users);
+		// make game in this room and add players
+		let game = new Game(room, players, io);
 
-    // make game in this room and add players
-    let game = new Game(room, players);
+		// make this game globally available by room-number so we can access it later
+		games[room] = game;
 
-    // make this game globally available by room-number so we can access it later
-    games[room] = game;
+		// from here on, the game logic is in the class, not the socket.io connection
+		games[room].start();
+	}
 
-    // from here on, the game logic is in the class, not the socket.io connection
-    games[room].start();
-  }
+	socket.onAny((event, data) => {
+		games[room][event](socket, data);
+	});
 
-  socket.on('figureLineUp', data => {
-    //Platziere die Figuren des Clients auf das Server Spielfeld
-    
-  });
-  
-  socket.on("playerEvent", (data) => {
-    // do whatever with the data
-
-    // examples for data:
-    // {"type": ANY TYPE YOU WANT TO ALLOW, "data": ANY DATA YOU WANT TO ALLOW}
-
-    // {"type": "move", "data": {"Piece":"ROCK","Position": [3,5]"}}
-    // Player sends event, event contains the player wanted to place something ("move"), he wants the piece to be a rock ("Piece") and the position he wants to place it ("Position")
-
-    console.log(data);
-    games[room].gameStep(socket, data);
-  })
-
-  socket.on('disconnect', socket => {
-
-    io.to(room).emit('Gegner verlassen');
-  });
+	socket.on('disconnect', socket => {
+		io.to(room).emit('Gegner verlassen');
+	});
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  	console.log('listening on *:3000');
 });
